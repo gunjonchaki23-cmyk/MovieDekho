c38cd64c4332fea38c1645565b98ef6bade05115
AIUB Student <student@aiub.edu>
2026-08-02 03:17:21 +0600

    Added links for Supergirl and The Odyssey

--- src/services/downloadLinks.js
+++ src/services/downloadLinks.js
@@ -11,4 +11,10 @@
 export const downloadLinks = {
   // Pink (2016) - Amitabh Bachchan
   "415358": "https://drive.google.com/file/d/1SPqaTX5CJRjxSpnmTr2qbuhiXhAIb2Ej/view?usp=drive_link",
+  
+  // Supergirl (2026)
+  "1081003": "https://drive.google.com/file/d/1Ds2iMRB16amTjx__xAy-yUh651Ht8Cma/view?usp=drive_link",
+
+  // The Odyssey (2026)
+  "1368337": "https://drive.google.com/file/d/1Kz0K-08yzeH0UUUnelE_tM-_v1BeOXUG/view?usp=drive_link"
 };
