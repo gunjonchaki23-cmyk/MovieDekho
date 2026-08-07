5b119439b5d727ef31ccd71d83e7dabd0b342ca5
AIUB Student <student@aiub.edu>
2026-08-07 14:45:18 +0600

    Removed sandbox attribute as vidlink blocked it

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -88,7 +88,6 @@ const MovieDetails = ({ movie, onClose, isSaved, onToggleSave, onSimilarSelect }
                 allowFullScreen
                 frameBorder="0"
                 className="stream-iframe"
-                sandbox="allow-scripts allow-same-origin allow-presentation"
               ></iframe>
             </div>
             
